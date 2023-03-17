adf1f7e23c89a259dde1336189d83f9cc28373fb
chandu <chandu@gmail.com>
2023-03-17 17:51:06 +0530

    first commit

--- 5_mutliplearray.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash  
-  
-# Reading multiple inputs using an array  
-  
-echo "Enter names : "  
-read -a names  
-echo "The entered names are : ${names[0]}, ${names[1]}."
--- BASH-SCRIPTS/first.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Printing hello world
-
-echo "hello world"
